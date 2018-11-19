36f9fb8ccd8e108be3b5f2b8ce9f953904da653d
joshuadesmith <joshuadesmith@utexas.edu>
2018-11-19 17:50:16 -0600

    add admin functionality for Product objects

--- productsearch/products/admin.py
+++ productsearch/products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    save_on_top = True
+
+    list_display = (
+        'id', 'description', 'last_sold', 'department', 'price',
+    )
+
+    list_display_links = (
+        'id', 'description',
+    )
+
+    list_filter = (
+        'department', 'unit',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
