4f8a8a9b68299993c63af28dce7a4bb2ee52958f
joshuadesmith <joshuadesmith@utexas.edu>
2018-11-20 10:31:00 -0600

    add advanced search template and view (needs work)

--- productsearch/products/urls.py
+++ productsearch/products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/', views.search, name='search'),  # TODO
+    path('search/', views.search, name='search'),
+    path('search/advanced/', views.advanced_search, name='advanced_search'),
 ]
--- productsearch/products/views.py
+++ productsearch/products/views.py
@@ -16,9 +16,26 @@ def search(request):
     if request.method == 'GET':
         search_string = request.GET.get('search_text')
         if search_string:
-            print("Got search string: {}".format(search_string))
             qs = qs.filter(description__icontains=search_string)
 
     template = loader.get_template('products/index.html')
+
+    return HttpResponse(template.render({'product_list': qs}, request))
+
+
+def advanced_search(request):
+    qs = Product.objects.order_by('-last_sold')
+    if request.method == 'GET':
+        print(request.GET)
+        desc = request.GET.get('description')
+        dep = request.GET.get('department')
+
+        if desc:
+            qs = qs.filter(description__icontains=desc)
+
+        if dep:
+            qs = qs.filter(department__icontains=dep)
+
+    template = loader.get_template('products/advsearch.html')
 
     return HttpResponse(template.render({'product_list': qs}, request))
